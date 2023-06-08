e575c04892a9c4bd8d7b0f7ca498003c5f76aad5
Oluwatobi Gbadamosi <tobbybosch@gmail.com>
2023-06-07 21:57:00 -0700

    new update

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,5 @@
+st_digit(number):
+        '''prints the last digit of a number'''
+            last_digit = abs(number) % 10
+                print(f"{last_digit}", end='')
+                    return last_digit
