0bb2d7f840000983ee80be08bd057da51f243953
Oluwatobi Gbadamosi <tobbybosch@gmail.com>
2023-09-12 04:29:03 -0700

    update JavaScript warmup

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
-#/usr/bin/node
-const count = process.argv.lengh;
-console.log(count ===2 ? 'No argement' : count ===3 ? 'Argument found' : 'Arguments found');
+#!/usr/bin/node
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
